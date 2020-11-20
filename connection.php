385d9b20c16ea51d2c0201e3ee11b6e82d12980a
aaran99 <74746475+aaran99@users.noreply.github.com>
2020-11-20 09:52:29 +0500

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 
-	$db=mysqli_connect("us-cdbr-east-02.cleardb.com","b08fe2958e1ba1","d5e0b1ca","library");  
+	$db=mysqli_connect("us-cdbr-east-02.cleardb.com","b08fe2958e1ba1","d5e0b1ca","heroku_ae6212804093e63");  
 					/* server name, username, password, database name */
 
 ?>
