48c5d88ae3b40252b827d84a8a6eaecef9388317
aaran99 <74746475+aaran99@users.noreply.github.com>
2020-11-20 09:14:51 +0500

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 
-	$db=mysqli_connect("localhost","root","","library");  
+	$db=mysqli_connect("us-cdbr-east-02.cleardb.com","b08fe2958e1ba1","d5e0b1ca","library");  
 					/* server name, username, password, database name */
 
 ?>
